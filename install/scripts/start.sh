7e32f3b9d4adf2ff4a48da08006b42ede571099f
rhoerbe <rainer@hoerbe.at>
2019-03-15 18:18:33 +0100

    add restart options to /opb/bin/start.sh

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -6,9 +6,38 @@ function main {
         echo "must start shibd and httpd as root"
         exit 1
     fi
-    cleanup_and_prep_shibd
-    start_shibd
-    start_httpd
+    get_commandline_opts $@
+    if [[ "$restart_httpd" ]]; then
+        restart_httpd
+    elif [[ "$restart_shibd" ]]; then
+        restart_shibd
+    else
+        cleanup_and_prep_shibd
+        start_shibd
+        start_httpd
+    fi
+}
+
+
+get_commandline_opts() {
+    while getopts ':HS' opt; do
+      case $opt in
+        H) restart_httpd='True';;
+        S) restart_shibd='True';;
+        *) echo "usage: $0 OPTIONS
+           Start shibd and httpd
+
+           OPTIONS:
+           -H  restart httpd only (graceful)
+           -S  restart shibd only
+           "; exit 0;;
+      esac
+    done
+    shift $((OPTIND-1))
+    if [[ "$restart_httpd" && "$restart_shibd" ]]; then
+        echo "-S and -H are mutually exclusive"
+        exit 1
+    fi
 }
 
 
@@ -31,6 +60,15 @@ function start_shibd {
 }
 
 
+function restart_shibd {
+    echo "restarting shibd"
+    kill $(cat /var/run/shibboleth/shib.pid)
+    sleep 2
+    export LD_LIBRARY_PATH=/opt/shibboleth/lib64
+    /usr/sbin/shibd -u shibd -g root -p /var/run/shibboleth/shib.pid
+}
+
+
 function start_httpd {
     echo "starting httpd"
     # `docker run` 1.12.6 will reset ownership and permissions on /run/httpd; therefore it need to be done again:
@@ -42,10 +80,16 @@ function start_httpd {
     # logging to stderr requires httpd to start as root (inside docker as of 17.05.0-ce)
     # pidfile in /run/httpd requires added kernel capabilities -> move to /var/log
     rm -f /run/httpd/* /var/log/httpd/httpd.pid 2>/dev/null || true
-    httpd -t -d /etc/httpd/ -f conf/httpd.conf
-    httpd -DFOREGROUND -d /etc/httpd/ -f conf/httpd.conf
-
+    /usr/sbin/httpd -t -d /etc/httpd/ -f conf/httpd.conf
+    /usr/sbin/httpd -DFOREGROUND -d /etc/httpd/ -f conf/httpd.conf
 }
 
 
-main
+function restart_httpd {
+    echo "restarting httpd"
+    /usr/sbin/httpd -d /etc/httpd/ -f conf/httpd.conf -k graceful
+}
+
+
+main $@
+
